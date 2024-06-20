1356abfdfc6c2e781dff13fe0ddf7f82d5569383
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-20 14:17:09 +0530

    select loop

---
+++ 11_loops/select_loops/example_2.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/bash
+
+# select loop is often used with case statements
+# select loop first of all return whole list with numbers to enter for user
+select name in mark john tom ben
+do
+    case $name in
+    mark)
+        echo "mark selected";;
+    john)
+        echo "john selected";;
+    tom)
+        echo "tom selected";;
+    ben)
+        echo "ben selected";;
+    *)
+      echo "Error. please provide the no between 1..4";;
+    esac
+done
+
+
+# Below will be the output. it ask for user input to execute the command in do
+#1) mark
+#2) john
+#3) tom
+#4) ben
+##? 2
+#john selected
---
+++ 11_loops/select_loops/syntax.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+# Different syntaxes
+
+select varName in list
+do
+    command1
+    command2
+    ....
+    ....
+    commandN
+done
